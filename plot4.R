d4571a5626263e925b1c2a1448c17839fc33edd8
Luefescor <luefescor@gmail.com>
2016-08-22 00:07:49 -0500

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -1,22 +1,22 @@
 
-
-#First of all i
+# Loading the dataset 
 fileData <- "household_power_consumption.txt"
 data <- read.table(fileData,
                    header=TRUE,
                    sep=";",
                    colClasses=c("character", "character", rep("numeric",7)),
                    na="?")
-# convert date and time variables to Date/Time class
+# Converting date and time variables to Date/Time class
 data$Time <- strptime(paste(data$Date, data$Time), "%d/%m/%Y %H:%M:%S")
 data$Date <- as.Date(data$Date, "%d/%m/%Y")
-# only use data from the dates 2007-02-01 and 2007-02-02
+# Indicating only use data from the dates 2007-02-01 and 2007-02-02
 dates <- as.Date(c("2007-02-01", "2007-02-02"), "%Y-%m-%d")
 data <- subset(data, Date %in% dates)
 
+# Creating plot4 formed by 4 elements
 png("plot4.png", width=480, height=480)
 
-par(mfrow=c(2,2))
+par(mfrow=c(2,2)) 
 # 1
 plot(data$Time, data$Global_active_power,
      type="l",
